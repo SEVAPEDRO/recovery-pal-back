df708957c64e788c5985a9334ce58946e17ec091
Federico <federicodiazgerstner@gmail.com>
2022-10-29 00:41:54 -0300

    Update de packages y controller para subir video a Cloudinary

--- routes/api/uploadFile.route.js
+++ routes/api/uploadFile.route.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 const storage = require("../../utils/multer");
 const videoController = require("../../controllers/Video.controller");
 
-router.get('/test', function(req, res, next) {
-    res.send('Llegaste a la ruta de  api/Routine.routes');
-  });
+router.get("/test", function (req, res, next) {
+  res.send("Llegaste a la ruta de  api/Routine.routes");
+});
 
-
-router.post('/uploadVideo', storage.single('video'), videoController.uploadVideo);
+router.post(
+  "/uploadVideo",
+  storage.single("video"),
+  videoController.uploadVideo
+);
 
 module.exports = router;
